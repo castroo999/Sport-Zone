21bc80e4e0f648b01a6aa245d997e8d179e9f073
castro <gc557686@gmail.com>
2026-03-17 23:18:52 -0300

    adicionando novos produtos e ajustando layout

--- sport/src/App.jsx
+++ sport/src/App.jsx
@@ -1,12 +1,28 @@
+import { PiSneakerMoveBold } from "react-icons/pi";
+import './App.css'
 import Header from "./components/Header"
-import Navbar  from "./components/Navbar"
+import Navbar from "./components/Navbar"
 import Carossel from "./components/Carossel"
+import CardLoja from "./components/CardLoja"
+import { catalogo } from "./data.js/node"
+
 function App() {
   return (
     <>
-    <Header />
-    <Navbar/>
-    <Carossel/>
+      <Header />
+      <Navbar />
+      <Carossel />
+      <h1><center>TENIS  <PiSneakerMoveBold size={40}/></center></h1>
+      <div className="lista-cards">
+        {catalogo.map((item) => (
+          <CardLoja
+            key={item.id}
+            title={item.title}
+            category={item.category}
+            banner={item.banner}
+          />
+        ))}
+      </div>
     </>
   )
 }
--- sport/src/components/CardLoja.jsx
+++ sport/src/components/CardLoja.jsx
@@ -1,8 +1,21 @@
+
 import './CardLoja.css'
 import {ShoppingCart} from 'lucide-react'
 
-export default function Cards(){
+export default function Cards({title, category, banner, onClick}){
     return(
-        false
+        <div className="card-loja" onClick={onClick}>
+            <div className="img-box">
+                <img src={banner} alt={title} className="card-img"/>
+
+                <div className="card-info">
+                    <h4>{title}</h4>
+                    <p>{category}</p>
+                    <button className='play-button'>
+                        <ShoppingCart size={14}/>
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
