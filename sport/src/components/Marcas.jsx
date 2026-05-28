41a5e351a134697b921a0546ecd85cd835cecb85
cafe pele <gustavo.castro.senai404@gmail.com>
2026-05-28 09:03:03 -0300

    arrumando marcas.jsx

--- sport/src/components/Marcas.jsx
+++ sport/src/components/Marcas.jsx
@@ -8,7 +8,7 @@ export default function Marcas({ setMarcaSelecionada, setCategoriaSelecionada })
   return (
     <div className="marcas">
       <img
-        src={logoadidas} onClick={() => {setMarcaSelecionada("Nike"); setCategoriaSelecionada("");}}/>
+        src={logoadidas} onClick={() => {setMarcaSelecionada("Adidas"); setCategoriaSelecionada("");}}/>
       <img src={nike} onClick={() => {setMarcaSelecionada("Nike"); setCategoriaSelecionada("")}} />
       <img src={olympikus} onClick={() => {setMarcaSelecionada("Olympikus"); setCategoriaSelecionada("")}} />
       <img src={mizuno} onClick={() => {setMarcaSelecionada("Mizuno"); setCategoriaSelecionada("")}} />
